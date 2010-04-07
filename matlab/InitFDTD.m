cf9969b5dcd1c4630e30f37ee94e47c4e3c387eb
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2010-04-07 16:31:35 +0200

    update matlab docu

--- matlab/InitFDTD.m
+++ matlab/InitFDTD.m
@@ -1,4 +1,14 @@
 function FDTD = InitFDTD(NrTS, endCrit, varargin)
+% function FDTD = InitFDTD(NrTS, endCrit, varargin)
+%
+% possible arguments for useage with openEMS:
+%   OverSampling:   nyquist oversampling of time domain dumps
+%
+% e.g FDTD = InitFDTD(5e5,1e-6,'OverSampling',10)
+%
+% openEMS matlab interface
+% -----------------------
+% author: Thorsten Liebig
 
 FDTD.ATTRIBUTE.NumberOfTimesteps=NrTS;
 FDTD.ATTRIBUTE.endCriteria=endCrit;
