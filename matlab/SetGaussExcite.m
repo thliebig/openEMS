a888c436071ea3e15121710fa42c9b7274ffc8c2
Thorsten Liebig <thorsten.liebig@gmx.de>
2010-04-24 13:09:24 +0200

    matlab interface: added some excitation docu

--- matlab/SetGaussExcite.m
+++ matlab/SetGaussExcite.m
@@ -1,4 +1,16 @@
-function FDTD = SetGaussExcite(FDTD,f0,fc);
+function FDTD = SetGaussExcite(FDTD,f0,fc)
+% function FDTD = SetSinusExcite(FDTD,f0,fc);
+%
+% f0 : center frequency
+% fc : 3dB cutoff frequency --> bandwidth is 2*fc
+%
+% see also SetSinusExcite SetCustomExcite
+%
+% e.g FDTD = SetGaussExcite(FDTD,1e9,1e8);
+%
+% openEMS matlab interface
+% -----------------------
+% author: Thorsten Liebig
 
 FDTD.Excitation.ATTRIBUTE.Type=0;
 FDTD.Excitation.ATTRIBUTE.f0=f0;
