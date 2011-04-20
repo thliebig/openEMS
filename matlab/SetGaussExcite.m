3bce66a510da091189977de20a42f349a69dd0a1
Sebastian Held <sebastian.held@uni-due.de>
2011-04-20 14:22:22 +0200

    typo

--- matlab/SetGaussExcite.m
+++ matlab/SetGaussExcite.m
@@ -1,5 +1,5 @@
 function FDTD = SetGaussExcite(FDTD,f0,fc)
-% function FDTD = SetSinusExcite(FDTD,f0,fc);
+% function FDTD = SetGaussExcite(FDTD,f0,fc);
 %
 % f0 : center frequency
 % fc : 3dB cutoff frequency --> bandwidth is 2*fc
