a9493f81386fa0507c4915ab4b1f819e6c0f9fcd
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-01-16 10:12:01 +0100

    Matlab: use InitFDTD with 'CoordSystem',1 instead ot InitCylindricalFDTD

--- matlab/InitCylindricalFDTD.m
+++ matlab/InitCylindricalFDTD.m
@@ -4,10 +4,15 @@ function FDTD = InitCylindricalFDTD(NrTS, endCrit, varargin)
 % see also InitFDTD
 %
 % e.g FDTD = InitCylindricalFDTD(5e5,1e-6,'OverSampling',10)
+% 
+% WARNING: This function is depreciated, use InitFDTD with 'CoordSystem',1
+%          e.g.: InitFDTD(5e5,1e-6,'OverSampling',10, 'CoordSystem',1)
 %
 % openEMS matlab interface
 % -----------------------
 % author: Thorsten Liebig
+
+warning('InitCylindricalFDTD: This function is depreciated, use InitFDTD with ''CoordSystem'',1');
 
 FDTD = InitFDTD(NrTS, endCrit, varargin{:});
 
--- matlab/InitFDTD.m
+++ matlab/InitFDTD.m
@@ -3,6 +3,7 @@ function FDTD = InitFDTD(NrTS, endCrit, varargin)
 %
 % possible arguments for useage with openEMS:
 %   OverSampling:   nyquist oversampling of time domain dumps
+%   CoordSystem:    choose coordinate system (0 Cartesian, 1 Cylindrical)
 %
 % e.g FDTD = InitFDTD(5e5,1e-6,'OverSampling',10)
 %
@@ -13,7 +14,11 @@ function FDTD = InitFDTD(NrTS, endCrit, varargin)
 FDTD.ATTRIBUTE.NumberOfTimesteps=NrTS;
 FDTD.ATTRIBUTE.endCriteria=endCrit;
 
-for n=1:(nargin-2)/2
-    FDTD.ATTRIBUTE.(varargin{2*n-1})=varargin{2*n};
+for n=1:numel(varargin)/2
+    if strcmp(varargin{2*n-1},'CoordSystem')==1
+        FDTD.ATTRIBUTE.CylinderCoords=varargin{2*n}==1;
+    else
+        FDTD.ATTRIBUTE.(varargin{2*n-1})=varargin{2*n};
+    end
 end
 
