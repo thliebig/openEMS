97311fc1a382e0df2909203a5dedc615a32a9b05
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-08-02 13:33:55 +0200

    matlab: help doc on WriteOpenEMS

--- matlab/WriteOpenEMS.m
+++ matlab/WriteOpenEMS.m
@@ -1,6 +1,15 @@
 function WriteOpenEMS(filename, FDTD, CSX)
 % function WriteOpenEMS(filename, FDTD, CSX)
-% 
+%
+% Write the FDTD and CSX structures to a file.
+%
+% example:
+% CSX = InitCSX();
+% FDTD = InitFDTD();
+% WriteOpenEMS('test.xml',FDTD,CSX)
+%
+% See also InitFDTD InitCSX CSXGeomPlot
+%
 % openEMS matlab interface
 % -----------------------
 % author: Thorsten Liebig
