10694249140ddebdc0d1f57ffb496de3fe28f802
Thorsten Liebig <thorsten.liebig@gmx.de>
2012-12-09 22:06:00 +0100

    matlab: change in ReadUI handling the auto regressive model
    Signed-off-by: Thorsten Liebig <thorsten.liebig@gmx.de>

--- matlab/ReadUI.m
+++ matlab/ReadUI.m
@@ -1,25 +1,29 @@
-function UI = ReadUI(files, path, freq, AR_order)
-% function UI = ReadUI(files, path, freq, AR_order)
+function UI = ReadUI(files, path, freq, varargin)
+% function UI = ReadUI(files, path, freq, varargin)
 %
 % read current and voltages from multiple files found in path
 %
 % returns voltages/currents in time and frequency-domain
-% 
+%
 % remarks on the frequency-domain:
 % - all signals are assumed to start at t=0
 % - currents that e.g. start at t = +delta_t/2 will be phase shifted by
 %    exp(-j*w*t(1))
 %
-% parameter:
-% freq (optional):
-%     frequency-domain values will be calculated according to 'freq'
-%     if 'freq' is not given, a FFT will be used
-% AR_order (optional):
-%     order to use within an auto-regressive model to improve FD accuracy
+% optional parameter:
+% freq: frequency-domain values will be calculated according to 'freq'
+%       if 'freq' is not given, a (zero padded) FFT will be used
 %
-% e.g.
+% optional key,value pairs:
+% 'AR'  : auto-regressive model to improve FD accuracy
+%         values: order to use within an AR model or 'auto'
+%
+% % examples:
 % U = ReadUI({'ut1_1','ut1_2'},'tmp' );
 % I = ReadUI('it1'            ,'tmp',[0.5e9 1e9 1.5e9]);
+% 
+% % using the auto-regressive model
+% U = ReadUI('port_ut1' , 'tmp', 'AR', 'auto');
 % 
 % openEMS matlab interface
 % -----------------------
@@ -31,8 +35,14 @@ if (nargin<2)
     path ='';
 end
 
-if (nargin<4)
-    AR_order = 0;
+AR_order = 0;
+
+for n=1:2:numel(varargin)
+    if (strcmp(varargin{n},'AR')==1)
+        AR_order =  varargin{n+1};
+    else
+        warning('CSXCAD:ReadUI', ['"' varargin{n} '" is an unknown argument']);
+    end
 end
 
 if (ischar(files))
@@ -59,10 +69,29 @@ for n=1:numel(filenames)
         [UI.FD{n}.f,UI.FD{n}.val] = FFT_time2freq( t,val );
     else
         UI.FD{n}.f = freq;
-        if (AR_order<=0)
+        if strcmpi(AR_order,'auto')
+            AR_order = 2;
+            EC = -1;
+            while 1
+                [val_ar t_ar UI.FD{n}.val EC] = AR_estimate( t, val, freq, AR_order);
+                if (EC==11)
+                    AR_order = AR_order*2;
+                else
+                    break;
+                end
+            end
+            if (EC~=0)
+                warning('CSXCAD:ReadUI','AR estimation failed, skipping...')
+                UI.FD{n}.val = DFT_time2freq( t, val, freq );
+            end
+        elseif (AR_order<=0)
             UI.FD{n}.val = DFT_time2freq( t, val, freq );
         else
-            [val_ar t_ar UI.FD{n}.val] = AR_estimate( t, val, freq, AR_order);
+            [val_ar t_ar UI.FD{n}.val EC] = AR_estimate( t, val, freq, AR_order);
+            if (EC~=0)
+                warning('CSXCAD:ReadUI','AR estimation failed, skipping...')
+                UI.FD{n}.val = DFT_time2freq( t, val, freq );
+            end
         end
     end   
 end
