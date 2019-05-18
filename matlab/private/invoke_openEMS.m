133f6ac976a2dbbb5349100062b6997617529d18
Andy <andy@desktop.fritz.box>
2019-05-18 21:00:47 +0200

    RunOpenEMS will now propagate errors from openems.exe, instead of silently failing

--- matlab/private/invoke_openEMS.m
+++ matlab/private/invoke_openEMS.m
@@ -44,4 +44,9 @@ else
     command = [command ' > ' logfile ' 2>&1'];
 end
 
-system(command);
+
+[exitcode, shelloutput] = system(command);
+if (exitcode~=0);
+	disp(shelloutput);
+	error(['openEMS.exe exited with error-code ' num2str(exitcode)]);
+end
