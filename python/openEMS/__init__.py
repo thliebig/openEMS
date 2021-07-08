f9c8954ed3e7841aec760b3b27396f52dd3a3eba
Thorsten Liebig <liebig@imst.de>
2021-07-08 15:14:30 +0200

    python: improve init on windows
    CSXCAD adds the dll path as needed on windows install version
    
    Signed-off-by: Thorsten Liebig <liebig@imst.de>

--- python/openEMS/__init__.py
+++ python/openEMS/__init__.py
@@ -3,4 +3,7 @@
 # Shortcut openEMS import
 from __future__ import absolute_import
 
+# try to import CSXCAD, e.g. make sure the (windows) dll path are set
+import CSXCAD
+
 from openEMS.openEMS import openEMS
