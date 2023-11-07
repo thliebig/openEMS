0515db85c9d01e88420bca3d65dce52c0d706e25
G. L <warinspacegame@gmail.com>
2023-11-07 21:39:33 +0200

    Update copyright notice

--- FDTD/extensions/engine_ext_lumpedRLC.cpp
+++ FDTD/extensions/engine_ext_lumpedRLC.cpp
@@ -1,6 +1,6 @@
 /*
 *	Additional
-*	Copyright (C) 2010 Thorsten Liebig (Thorsten.Liebig@gmx.de)
+*	Copyright (C) 2023 Gadi Lahav (gadi@rfwithcare.com)
 *
 *	This program is free software: you can redistribute it and/or modify
 *	it under the terms of the GNU General Public License as published by
