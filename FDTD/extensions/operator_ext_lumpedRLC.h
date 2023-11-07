1cc55da7c4c581e3466f339fad149ef9af55428d
G. L <warinspacegame@gmail.com>
2023-11-07 21:41:28 +0200

    Update copyright notice

--- FDTD/extensions/operator_ext_lumpedRLC.h
+++ FDTD/extensions/operator_ext_lumpedRLC.h
@@ -1,9 +1,19 @@
 /*
- * operator_ext_lumpedRLC.h
- *
- *  Created on: 15 Jun 2023
- *      Author: Gadi Lahav
- */
+*	Copyright (C) 2023 Gadi Lahav (gadi@rfwithcare.com)
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef OPERATOR_EXT_LUMPEDRLC_H_
 #define OPERATOR_EXT_LUMPEDRLC_H_
