e872e027e98eb8a1a780313717957317fb54a417
Matias Senger <m.senger@hotmail.com>
2024-10-23 21:45:10 +0200

    Removed hardcoded path and improved error handling

--- python/setup.py
+++ python/setup.py
@@ -1,23 +1,24 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 from pathlib import Path
+import os
 import sys
 
-# Hardcoded paths that would be better to remove but I don't know how to:
-PATH_TO_OPEN_EMS_INSTALLATION = Path(__file__).parent.parent.parent.parent
+# TODO remove hardcoded path:
 PATH_TO_CSXCAD_PYTHON_PACKAGE = Path(__file__).parent.parent.parent/'CSXCAD/python'
 
 # Define some functions to check that the packages are installed where we expect them to be installed, and raise meaningful errors otherwise:
-def check_that_folder_looks_like_openEMS_installation(path_to_some_folder:Path):
+def looks_like_openEMS_is_installed_there(path_to_some_folder:Path):
     path_to_some_folder = Path(path_to_some_folder)
     FILES_THAT_SHOULD_EXIST = [
         path_to_some_folder/'bin/openEMS',
         path_to_some_folder/'include/openEMS/openems.h',
     ]
     if any([not p.is_file() for p in FILES_THAT_SHOULD_EXIST]):
-        raise RuntimeError(f'I was expecting to find the installation of openEMS in {path_to_some_folder}, but it does not look like it is installed there. Please provide the path to where you have it installed and write it in `PATH_TO_OPEN_EMS_INSTALLATION` in the file {Path(__file__).resolve()}. Should you have it installed there, please create an issue in https://github.com/thliebig/openEMS reporting this. ')
+       return False
+    return True
 
-def check_that_folder_looks_like_CSXCAD_python_package(path_to_some_folder:Path):
+def looks_like_CSXCAD_python_package(path_to_some_folder:Path):
     path_to_some_folder = Path(path_to_some_folder)
     FILES_THAT_SHOULD_EXIST = [
         path_to_some_folder/'pyproject.toml',
@@ -25,13 +26,19 @@ def check_that_folder_looks_like_CSXCAD_python_package(path_to_some_folder:Path)
         path_to_some_folder/'CSXCAD/CSXCAD.pyx',
     ]
     if any([not p.is_file() for p in FILES_THAT_SHOULD_EXIST]):
-        raise RuntimeError(f'I was expecting to find the python package CSXCAD in {path_to_some_folder}, but it does not look like it is there. Please provide the path to where you have it installed and write it in `PATH_TO_CSXCAD_PYTHON_PACKAGE` in the file {Path(__file__).resolve()}. Should you have it installed there, please create an issue in https://github.com/thliebig/CSXCAD reporting this. ')
+        return False
+    return True
 
-# Check that the packages are installed there:
-check_that_folder_looks_like_openEMS_installation(PATH_TO_OPEN_EMS_INSTALLATION)
-check_that_folder_looks_like_CSXCAD_python_package(PATH_TO_CSXCAD_PYTHON_PACKAGE)
+if 'OPENEMS_INSTALL_PATH' not in os.environ:
+    raise SystemExit('Please set the environment variable OPENEMS_INSTALL_PATH to point to where openEMS was installed. ')
 
-# Perform the actual installation:
+path_to_openEMS_installation = Path(os.environ['OPENEMS_INSTALL_PATH']) # Environment variables are always strings, so this should never raise any error.
+
+if not looks_like_openEMS_is_installed_there(path_to_openEMS_installation):
+    raise SystemExit(f'I was expecting to find openEMS installed in {path_to_openEMS_installation}, but it does not look like it is installed there. ')
+
+if not looks_like_CSXCAD_python_package(PATH_TO_CSXCAD_PYTHON_PACKAGE):
+    raise SystemExit(f'Cannot find python module of CSXCAD in {PATH_TO_CSXCAD_PYTHON_PACKAGE}. Please modify the `PATH_TO_CSXCAD_PYTHON_PACKAGE` variable in this setup.py file, which is located in {Path(__file__).resolve()}, to point to wherever you have the CSXCAD python package. ')
 
 sys.path.append(str(PATH_TO_CSXCAD_PYTHON_PACKAGE))
 
@@ -39,9 +46,9 @@ extensions = [
     Extension(
         '*', 
         ['openEMS/*.pyx',],
-        include_dirs = [str(Path(PATH_TO_OPEN_EMS_INSTALLATION)/'include')],
-        library_dirs = [str(Path(PATH_TO_OPEN_EMS_INSTALLATION)/'lib')],
-        runtime_library_dirs = [str(Path(PATH_TO_OPEN_EMS_INSTALLATION)/'lib')],
+        include_dirs = [str(path_to_openEMS_installation/'include')],
+        library_dirs = [str(path_to_openEMS_installation/'lib')],
+        runtime_library_dirs = [str(path_to_openEMS_installation/'lib')],
         language = 'c++',
         libraries = ['openEMS','nf2ff'],
     )
