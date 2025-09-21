d0c76927a822ce9f68a9cfdefbfd87ffeea9df67
gadiLhv <gadi.lhv@gmail.com>
2025-09-21 10:14:46 +0300

    Forgot to approve processmodematch.h

--- Common/processmodematch.h
+++ Common/processmodematch.h
@@ -54,7 +54,7 @@ public:
 	virtual int GetNumberOfIntegrals() const {return 2;}
 	virtual double* CalcMultipleIntegrals();
 
-protected:
+protected:ProcessModeMatch
 	//normal direction of the mode plane
 	int m_ny;
 
