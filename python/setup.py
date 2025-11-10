8ee53425cbe8eb588da37ffd1d5155c9bc81ab80
Yifeng Li <tomli@tomli.me>
2025-11-10 18:33:19 +0000

    python/setup.py: add library paths automatically.
    Currently, building the openEMS Python extension requires users
    to manually inputs all include and library paths explicitly.
    For a typical installation, this can be rather cumbersome.
    
    Suppose openEMS is installed to "$HOME/opt", and some custom
    libraries also exist in `/usr/local` (which, for some reason,
    is not listed in a system's search path). The required command
    is:
    
        python3 setup.py build_ext \
            -I "$HOME/opt/include:/usr/local/include" \
            -L "$HOME/opt/lib:/usr/local/lib" \
            -R $HOME/opt/lib
    
    Alternatively:
    
        export CXXFLAGS="-I$HOME/opt/include -I/usr/local/include"
        export LDFLAGS="-L$HOME/opt/lib -L/usr/local/lib -Wl,-rpath,/usr/local/lib"
        python3 setup.py build_ext
    
    This process is currently automated by scripts/build_python.sh
    in openEMS-Project.git. But the problem is better fixed at the
    Python module level.
    
    To simplify installation, this commit introduces a function named
    "determine_build_options()", which does the following things:
    
    1. If "OPENEMS_INSTALL_PATH" is set, automatically add
       "OPENEMS_INSTALL_PATH/include", "OPENEMS_INSTALL_PATH/lib", and
       "OPENEMS_INSTALL_PATH/lib{32,64}" (selected at runtime) to the
       compiler header path, library path, and rpath. As a special
       case, if a user incorrectly points OPENEMS_INSTALL_PATH to
       "OPENEMS_INSTALL_PATH/bin"
       (but "OPENEMS_INSTALL_PATH/bin/../include/openEMS" exists),
       remove "/bin".
    
    2. If "VIRTUAL_ENV" is set, treat it in the same way as
       "OPENEMS_INSTALL_PATH". This is because we recommends users
       to create a Python "venv" at the same C++ installation path,
       making both paths overlap. This way users don't need to remember
       two paths, and the prefix can be detected automatically if
       the "venv" is activated.
    
    3. As a special case, always treat "/usr/local" as a prefix too,
       because this prefix is too many on POSIX systems.
    
    4. As a special case, add "OPENEMS_INSTALL_PATH" itself as a library
       path on Windows.
    
    5. On all POSIX systems, set "-std=c++11" because some compilers
       don't enable it by default.
    
    Upon finishing, "determine_build_options()" returns a directionally
    that contains the keys "extra_compile_args", "include_dirs",
    "library_dirs", and "runtime_library_dirs". We can pass these arguments
    later to Extension().
    
    Signed-off-by: Yifeng Li <tomli@tomli.me>

--- python/setup.py
+++ python/setup.py
@@ -9,24 +9,140 @@ from setuptools import setup
 from setuptools import Extension
 from Cython.Build import cythonize
 
-import os, sys
+import sys
+import math
+import os
+import platform
+import subprocess
+import pathlib
+
+
 ROOT_DIR = os.path.dirname(__file__)
 
 sys.path.append(os.path.join(ROOT_DIR,'..','..','CSXCAD','python'))
 
-# Strictly speaking we should detect compiler, not platform,
-# unfortunately there's no easy way to do so without implementing
-# full compiler detection logic. This is good enough for 90% of
-# use cases.
-cxxflags = []
-if os.name == "posix":
-    cxxflags.append("-std=c++11")
 
+def normalize_path_subdir(path_str, subdir):
+    # We want to find the correct $CSXCAD_INSTALL_PATH even if the
+    # user input is slightly "incorrect".
+    #
+    # /home/opt/physics/venv -> /home/opt/physics (subdir == "venv")
+    # /home/opt/physics/bin -> /home/opt/physics (subdir == "bin")
+    path = pathlib.Path(path_str)
+    if path.stem == subdir and (path / "../include/openEMS").exists():
+        return (path / "../").resolve()
+    else:
+        return path
+
+
+def determine_build_options():
+    cpu_bits = str(int(math.log2(sys.maxsize) + 1))
+
+    local_prefix_list = []
+    use_prefix = True
+    user_entered_prefix = False
+
+    if "OPENEMS_INSTALL_PATH_IGNORE" in os.environ:
+        use_prefix = False
+
+    if use_prefix and 'OPENEMS_INSTALL_PATH' in os.environ:
+        # Because CSXCAD and openEMS are usually installed to a non-standard
+        # location such as the user's home directory rather than a standard
+        # directory, their paths must be told to the compilers explicitly.
+        # Since a general solution is impossible (the actual installation path
+        # is unpredictable), users should set OPENEMS_INSTALL_PATH to ensure a
+        # successful installation.
+        local_prefix_list.append(
+            normalize_path_subdir(os.environ["OPENEMS_INSTALL_PATH"], "bin")
+        )
+        user_entered_prefix = True
+    if 'VIRTUAL_ENV' in os.environ:
+        # if Python venv used, our documentation recommends using the same
+        # path as OPENEMS_INSTALL_PATH, so that the custom C++ and Python prefix
+        # overlaps.
+        local_prefix_list.append(
+            normalize_path_subdir(os.environ["VIRTUAL_ENV"], "venv")
+        )
+        user_entered_prefix = True
+
+    if use_prefix and (not user_entered_prefix):
+        raise RuntimeError(
+            "No environment variable OPENEMS_INSTALL_PATH or VIRTUAL_ENV found, "
+            "installation may fail due to missing headers and libraries! "
+            "Please set the environment variable OPENEMS_INSTALL_PATH to the path "
+            "of CSXCAD/openEMS installation, check documentation for details. "
+            "If you know what you're doing, set OPENEMS_INSTALL_PATH_IGNORE=1 to "
+            "suppress this error."
+        )
+
+    if platform.system() == "Darwin":
+        # In additional to libraries in $OPENEMS_INSTALL_PATH and $VIRTUAL_ENV, we
+        # also need to list custom headers and libraries installed to the local
+        # system but are not used by compilers by default (such as a custom Boost).
+        # On macOS, the required prefix are -L $(brew --prefix)/include and
+        # -R $(brew --prefix)/lib respectively. Hardcode it as a special treatment.
+        try:
+            status = subprocess.run(["brew", "--prefix"], capture_output=True)
+            path = status.stdout.decode("UTF-8").replace("\n", "")
+            local_prefix_list.append(
+                pathlib.Path(path)
+            )
+        except FileNotFoundError:
+            pass
+    if os.name == "posix":
+        # The path /usr/local is also too common on Unix systems, so we hardcode it
+        # as a special treatment on Unix-like systems. For example, on CentOS, the
+        # paths -L /usr/local/include and -R /usr/local/lib must be listed if a
+        # custom Boost is installed here.
+        local_prefix_list.append(pathlib.Path("/usr/local"))
+
+    build_options = {
+        "extra_compile_args": [],
+        "include_dirs": [],
+        "library_dirs": [],
+        "runtime_library_dirs": [],
+    }
+
+    for prefix in local_prefix_list:
+        prefix_path = pathlib.Path(prefix)
+        if os.name == 'nt':
+            build_options["library_dirs"] += [str(prefix_path)]
+
+        build_options["include_dirs"] += [
+            str(prefix_path / "include")
+        ]
+        build_options["library_dirs"] += [
+            str(prefix_path / "lib"),
+            str(prefix_path / "lib" / cpu_bits)
+        ]
+        build_options["runtime_library_dirs"] += [
+            str(prefix_path / "lib"),
+            str(prefix_path / "lib" / cpu_bits)
+        ]
+
+    # Strictly speaking we should detect compiler, not platform,
+    # unfortunately there's no easy way to do so without implementing
+    # full compiler detection logic. This is good enough for 90% of
+    # use cases.
+    if os.name == "posix":
+        build_options["extra_compile_args"].append("-std=c++11")
+
+    # Setting this will cause an exception during build on Windows platforms.
+    if os.name != "posix":
+        del build_options["runtime_library_dirs"]
+
+    return build_options
+
+
+build_options = determine_build_options()
 extensions = [
-    Extension("*", [os.path.join("openEMS","*.pyx")],
-        language="c++",             # generate C++ code
-        libraries    = ['CSXCAD','openEMS', 'nf2ff'],
-        extra_compile_args=cxxflags),
+    Extension(
+        name="*",
+        sources=[os.path.join("openEMS", "*.pyx")],
+        language="c++",
+        libraries=['CSXCAD', 'openEMS', 'nf2ff'],
+        **build_options
+    ),
 ]
 
 setup(
