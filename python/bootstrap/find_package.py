d0ba56ddaee7e2d83e043b3db2920feead1931ef
Yifeng Li <tomli@tomli.me>
2025-11-11 05:13:05 +0000

    python: enhanced CSXCAD detection.
    openEMS's Python extension depends on CSXCAD Python extension to build.
    In the legacy setup.py build_ext code, this problem is solved two ways.
    If CSXCAD Python extension has already been installed, calling setup.py's
    build_ext automatically uses the installed version. If not, setup.py
    adds "../../CSXCAD/python" into the sys search path, assuming "../../"
    points to the project root, "openEMS-Project".
    
    This fundamentally violates the design principle of the modern pip feature:
    build isolation. When build isolation is active (by default), pip creates an
    internal venv just for building the package, all pre-existing packages,
    including the system's global packages or the user's own packages (in ~/.local
    or venv) are completely ignored. After the package is built, it's installed
    as usually, and pip throws away the one-use venv.
    
    As a result, the pre-installed CSXCAD will be invisible to pip. The correct
    solution is to define CSXCAD in the "[build-system][requires]" field of
    pyproject.toml. However, this creates two more problems:
    
    1. It's possible to define "CSXCAD @ file://", but relative paths are
    forbidden by pip, so "../../CSXCAD/python/" won't work. It's currently
    not possible to use environment variables in pyproject.toml.
    
    2. It's possible to define "CSXCAD @ git+https://", but openEMS.git can
    be either a submodule from openEMS-Project.git, or an individual repo.
    Both repos may depend on incompatible versions of CSXCAD.
    
    To solve this problem, we extend setuptools to create a custom build
    backend known as "bootstrap/setuptools_build_meta_custom.py" to insert
    a dynamic CSXCAD dependency.
    
    First, if the environment variable CSXCAD_PYSRC_PATH is set, all checks
    are bypassed. We immediately return CSXCAD as an extra dependency with
    the user-specified path in the filesystem. If the path begins with "git+",
    we treat it as an arbitrary git path such as "git+https://example.com/x.git".
    
    Otherwise, We look for "../../update_openEMS.sh" and "../../CSXCAD/python/"
    relative to setup.py and relative to user's shell ($PWD). If both files
    exist, it means we're building from the full project repo as a submodule.
    We return the absolute path of "../../CSXCAD/python/" to pip as an extra
    build-time dependency.
    
    If we're unable to find both files, we determine whether we're building
    from the complete source code tree directly (pip 21.3+ uses in-tree build)
    or from a temporary directory that contains nothing but "openEMS/python"
    (older pip uses out-of-tree build in /tmp/pip-xxxxxx) by checking whether
    the file "../openems.h" exists. It only exists if the full source tree
    is accessible.
    
    If we are building in-tree, it means we're truly building an individual
    openEMS.git repository without the openEMS-Project repo. In this case,
    we return the GitHub repository path to CSXCAD.git.
    
    If we're building out-of-tree, it means the situation is ambiguous, and
    we cannot decide whether we're building inside openEMS-Project, or as an
    an individual openEMS.git repo. Instead of picking the potentially-
    incompatible GitHub CSXCAD.git repo, we throw a RuntimeError and display
    an error message to tell users to either disable build isolation (so the
    already-installed CSXCAD can be used), or to manually specify the path
    to CSXCAD Python extension's source code path.
    
    Signed-off-by: Yifeng Li <tomli@tomli.me>

---
+++ python/bootstrap/find_package.py
@@ -0,0 +1,104 @@
+# This file is used to find and insert dependencies dynamically.
+# It's used by both "setuptools_build_meta_custom.py" to find
+# build-time dependencies, and also by "setup.py" to find run-
+# time dependencies.
+import os
+from pathlib import Path
+
+
+def is_project_root(path_prefix):
+    # Can we find the following files?
+    file_list = ["update_openEMS.sh", "CSXCAD/python"]
+
+    for file in file_list:
+        if not (path_prefix / Path(file)).exists():
+            return False
+
+    # If so, it means we're build openEMS/python from the openEMS-Project
+    # directory, and so should point CSXCAD's path to:
+    # openEMS-Project/CSXCAD/python
+    return True
+
+
+def pip_in_tree_build():
+    # pip has two modes: in-tree build and out-of-tree build,
+    # depending on its version.
+    #
+    # Out-of-tree was the default for pip < 21.3, the Python
+    # source directory is copied to /tmp for a clean build, but
+    # this caused problems if the project uses anything from the
+    # parent directory (what we do here). For pip >= 21.3,
+    # out-of-tree builds are used instead.
+    #
+    # A way to distinguish them is to see whether an expected file
+    # from the parent directory exists.
+    if Path("../openems.h").exists():
+        return True
+    else:
+        return False
+
+
+def add_csxcad(version_str=""):
+    if "CSXCAD_PYSRC_PATH" in os.environ:
+        # User provides an explicit source path, just use it without
+        # trying anything else.
+        if os.environ["CSXCAD_PYSRC_PATH"].startswith("git+"):
+            return ["CSXCAD %s @ %s" % (version_str, os.environ["CSXCAD_PYSRC_PATH"])]
+        else:
+            path = Path(os.environ["CSXCAD_PYSRC_PATH"]).resolve()
+            # "file://localhost/" is usually written out as just
+            # "file:///", they're equivalent per RFC 8089. We use the
+            # full form here because pypa/packaging doesn't correctly
+            # handle "file:///" until v19.0.
+            #
+            # See:
+            # https://github.com/pypa/packaging/commit/eb0243865079c7c2179730da93130447556ea020
+            # https://github.com/pypa/pip/blob/7e49dca9277bf4e325b85cfb9ebe70401f194fb6/src/pip/_internal/utils/urls.py#L30
+            return ["CSXCAD %s @ file://localhost/%s" % (version_str, str(path))]
+
+    # User provides no path, let's try some heuristics now.
+    path = None
+
+    if is_project_root(Path("../../")):
+        # Find path relative to setup.py. Do we see a project root?
+        path = Path("../../CSXCAD/python/")
+    elif (
+        "PWD" in os.environ and
+        is_project_root(Path(os.environ["PWD"]) / "../../")
+    ):
+        # Find path relative to user shell. It's not reliable (user's
+        # shell must be in "openEMS/python" but pip can be invoked anywhere),
+        # but it's our last chance (e.g. on pip < 21.3 with out-of-tree
+        # builds).
+        path = Path(os.environ["PWD"]) / "../../CSXCAD/python/"
+
+    # found path
+    if path:
+        return ["CSXCAD %s @ file://localhost/%s" % (version_str, str(path))]
+
+    # path not found, try fallbacks
+    if pip_in_tree_build():
+        # We're confident that the user has nothing but an isolated openEMS.git
+        # repo, we instruct pip to download CSXCAD from scratch.
+        path = "git+https://github.com/thliebig/CSXCAD.git#subdirectory=python"
+        return ["CSXCAD %s @ %s" % (version_str, path)]
+    else:
+        # The pip version is new enough to support pyproject.toml and build
+        # backends, but old enough so it uses out-of-tree builds, preventing
+        # us from locating CSXCAD's source code. It's too ambiguous to make
+        # any decision: Are we trying to build openEMS itself, or build openEMS
+        # submodule within the openEMS-Project directory? Instead of returning
+        # the latest git version of CSXCAD as dependency, which is potentially
+        # incompatible, we raise an error.
+        raise RuntimeError(
+            "Unable to detect CSXCAD's Python source code path. "
+            "You're likely using an old pip without 'in-tree build' "
+            "support. You can pick one solution below: "
+            "(1) Rerun pip with 'pip install . --no-build-isolation' "
+            "if CSXCAD Python extension is already installed (recommended). "
+            "(2) Provide the path via CSXCAD_PYSRC_PATH and rerun pip "
+            "(e.g. 'export "
+            "CSXCAD_PYSRC_PATH=/home/user/openEMS-Project/CSXCAD/python/ && "
+            "pip install . '). "
+            "(3) Upgrade to pip 21.3 or newer."
+        )
