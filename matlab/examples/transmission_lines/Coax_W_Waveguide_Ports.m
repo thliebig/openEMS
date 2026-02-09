c56e4f215bc5aaa823b0e26853ac951b4a583798
gadiLhv <gadi.lhv@gmail.com>
2026-02-09 09:19:17 +0200

    Cleaned up test script

--- matlab/examples/transmission_lines/Coax_W_Waveguide_Ports.m
+++ matlab/examples/transmission_lines/Coax_W_Waveguide_Ports.m
@@ -3,19 +3,9 @@
 % a coaxial T-line.
 % (c) 2026 Gadi Lahav (gadi@rfwithcare.com)
 
-
 clc;
 clear;
 close all;
-
-function m = snapMesh(m,x0,x1)
-    m.x = unique([m.x x0(1) x1(1)]);
-    m.x = unique([m.x x0(1) x1(1)]);
-    m.y = unique([m.y x0(2) x1(2)]);
-    m.y = unique([m.y x0(2) x1(2)]);
-    m.z = unique([m.z x0(3) x1(3)]);
-    m.z = unique([m.z x0(3) x1(3)]);
-end
 
 % -------------------------------------------------------------------------
 % Simulation Setup
@@ -26,20 +16,10 @@ unit = 1e-3; % drawing unit in mm (adjust if necessary)
 f_start = 2e9;
 f_stop  = 3e9;
 
-% Coax geometry parameters
-%coax_D = 2 * unit;                  
-%coax_shield_thick = 0.15 * unit;
-%coax_wire_D = 0.5 * unit;
-%coax_L = 25 * unit;
-%
-%Airbox_Add = 0 * unit;
-
 coax_D = 2;                  
 coax_shield_thick = 0.15;
 coax_wire_D = 0.5;
-coax_L = 25;
-
-Airbox_Add = 0;
+coax_L = 25
 
 Mode_File_Path = '../../../python/Tests';
 E_Mode_File = 'Coax_Er.csv';
@@ -57,7 +37,7 @@ addpath('~/opt/openEMS/share/openEMS/matlab');
 
 physical_constants;
 
-FDTD = InitFDTD('NrTS', 30000, 'EndCriteria', 1e-4);
+FDTD = InitFDTD('NrTS', 300000, 'EndCriteria', 1e-4);
 f0 = 0.5*(f_start + f_stop);
 FDTD = SetGaussExcite(FDTD, f0, 0.5*(f_stop - f_start));
 
@@ -69,8 +49,6 @@ mesh_res = ((c0/f_stop)/unit)/100;
 
 % -------------------------------------------------------------------------
 % Initial mesh bounding box
-
-
 CSX = InitCSX();
 
 % -------------------------------------------------------------------------
@@ -92,7 +70,6 @@ CSX = SetMaterialProperty( CSX, 'PTFE', 'Epsilon', teflon_epsR );
 % Coax Inner conductor
 CSX = AddCylinder(CSX, 'PTFE', 10, [0 0 0], [0 0 coax_L], coax_D*0.5);
 
-
 % Add all relevant points to mesh
 mesh.x = [];
 mesh.y = [];
@@ -107,13 +84,10 @@ mesh.x = unique(mesh.x);
 mesh.y = mesh.x;
 mesh.z = unique([0:mesh_res*0.25:mesh_res (coax_L - fliplr(0:mesh_res*0.25:mesh_res)) coax_L]);
 
-
-
 % Smooth all meshes
 mesh.x = SmoothMeshLines(mesh.x, mesh_res, 1.25);
 mesh.y = SmoothMeshLines(mesh.y, mesh_res, 1.25);
 mesh.z = SmoothMeshLines(mesh.z, mesh_res, 1.25);
-
 
 % Define the grid inside CSX
 CSX = DefineRectGrid(CSX, unit, mesh);
@@ -145,7 +119,8 @@ stop_coax  = [(coax_D*0.5 + coax_shield_thick)*[ 1  1] mesh.z(3)];
 start_coax = [(coax_D*0.5 + coax_shield_thick)*[-1 -1] mesh.z(end - 1)];
 stop_coax  = [(coax_D*0.5 + coax_shield_thick)*[ 1  1] mesh.z(end - 2)];
 
-[CSX, port{2}] = AddWaveGuidePort(CSX, 15, 2, start_coax, stop_coax, 'z', '', '', kc , 1, 'E_WG_file', E_Mode_File, 'H_WG_file', H_Mode_File);
+[CSX, port{2}] = AddWaveGuidePort(CSX, 15, 2, start_coax, stop_coax, 'z', '', '', kc , 0, 'E_WG_file', E_Mode_File, 'H_WG_file', H_Mode_File);
+
 % -------------------------------------------------------------------------
 % Write XML and Run Simulation
 
@@ -168,22 +143,23 @@ RunOpenEMS( Sim_Path, Sim_CSX );
 % Post-processing
 
 freq = linspace(f_start, f_stop, 201);
-
-port = calcPort(port, Sim_Path, freq, 'RefImpedance', 50);
+port = calcPort(port, Sim_Path, freq, 'RefImpedance', Zw);
 
 % Calculate and plot S-parameters
 S11 = port{1}.uf.ref ./ port{1}.uf.inc;
 S21 = port{2}.uf.ref ./ port{1}.uf.inc;
 
-
 figure;
-plot(freq/1e9, 20*log10(abs(S11)), 'b-', 'LineWidth', 2);
-hold on;
-plot(freq/1e9, 20*log10(abs(S21)), 'r--', 'LineWidth', 2);
-xlabel('Frequency (GHz)');
-ylabel('S-Parameters (dB)');
-legend('S11', 'S21');
-title('Coax to Waveguide Test (Matlab)');
+plot(freq/1e9, 20*log10(abs([S11(:) S21(:)])),'linewidth',3);
+set(gca,'fontsize',16);
 grid on;
+title('Coax With Waveguide Ports');
+set(gcf,'position',[330    316   1028    553]);
+% Pause for plot to stabilize
+pause(0.1);
 
+xlabel('Frequency (GHz)','fontsize',20);
+ylabel('S-Parameters (dB)','fontsize',20);
 
+legHdl = legend('S11', 'S21');
+set(legHdl,'fontsize',18);
